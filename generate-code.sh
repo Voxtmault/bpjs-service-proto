f7e6e37d23fa6486dc197bf7ecf1c7b73e2e7589
Andy <voxtmault@gmail.com>
2024-09-08 16:29:42 +0700

    Initial Commit

---
+++ generate-code.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Generating gRPC Codes"
+
+# Ensure Go binaries are in PATH
+export PATH="$PATH:$(go env GOPATH)/bin"
+
+# Generate Go Codes
+protoc --go_out=./go --go-grpc_out=./go bpjs.proto
+echo "Go Codes Generated"
+
+# Generate JavaScript code
+protoc --js_out=import_style=commonjs,binary:./js bpjs.proto
+echo "JS Code Generated"
+
+# Generate gRPC-Web code
+protoc --grpc-web_out=import_style=commonjs,mode=grpcwebtext:./web-js bpjs.proto
+echo "JS Web Generated"
